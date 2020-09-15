3869f1d9c65d6384b1e034b866b36f0fadde7dcd
Cheyenne Wäny <ch.waeny@gmail.com>
2020-09-15 20:56:03 +0200

    changes & ue2

--- ue2/sketch.js
+++ ue2/sketch.js
@@ -1,57 +1,167 @@
 function setup() {
-  createCanvas(600, 600);
-  background(color(244, 243, 241));
+  createCanvas(850, 850);
+  background('#230243');
 }
 
 function draw() {
   noFill();
-  stroke(234, 87, 103);
 
-  let newSize = 25;
-  circle(300, 300, newSize);
+  stroke('#654d7b');
+  strokeWeight(1);
+  line(25,25,825,825);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+  let b = color('#ea5666');
+  let c = color('#f4f31');
+  let d = color('#c2e59c');
 
-  newSize += 25;
-  circle(300, 300, newSize);
+  let a1 = 25;
+  let a2 = 25;
+  stroke(b);
+  strokeWeight(1);
+  ellipse(a1, a2, a1, a2);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+  stroke('#654d7b');
+  strokeWeight(1);
+  line(25,25,825,825);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+  a2 += 25;
+  stroke(c);
+  strokeWeight(2);
+  ellipse(a1, a2, a1, a2);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+  a1 += 30;
+  stroke(d);
+  strokeWeight(3);
+  ellipse(a1, a2, a1, a2);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+  a2 += 35;
+  stroke(b);
+  strokeWeight(4);
+  ellipse(a1, a2, a1, a2);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+  a1 += 40;
+  stroke(c);
+  strokeWeight(3);
+  ellipse(a1, a2, a1, a2);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+  a2 += 45;
+  stroke(d);
+  strokeWeight(2);
+  ellipse(a1, a2, a1, a2);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+  a1 += 50;
+  stroke(b);
+  strokeWeight(1);
+  ellipse(a1, a2, a1, a2);
 
-  newSize += 25;
-  circle(300, 300, newSize);
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
+
+  a2 += 55;
+  stroke(c);
+  strokeWeight(2);
+  ellipse(a1, a2, a1, a2);
+
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
+
+  a1 += 60;
+  stroke(d);
+  strokeWeight(3);
+  ellipse(a1, a2, a1, a2);
+
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
+
+  a2 += 65;
+  stroke(b);
+  strokeWeight(1);
+  ellipse(a1, a2, a1, a2);
+
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
+
+  a1 += 70;
+  stroke(c);
+  strokeWeight(4);
+  ellipse(a1, a2, a1, a2);
+
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
+
+  a2 += 75;
+  stroke(d);
+  strokeWeight(3);
+  ellipse(a1, a2, a1, a2);
+
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
+
+  a1 += 80;
+  stroke(b);
+  strokeWeight(2);
+  ellipse(a1, a2, a1, a2);
+
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
+
+  a2 += 85;
+  stroke(c);
+  strokeWeight(1);
+  ellipse(a1, a2, a1, a2);
+
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
+
+  a1 += 90;
+  stroke(d);
+  strokeWeight(2);
+  ellipse(a1, a2, a1, a2);
+
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
+
+  a2 += 95;
+  stroke(b);
+  strokeWeight(3);
+  ellipse(a1, a2, a1, a2);
+
+    stroke('#654d7b');
+    strokeWeight(1);
+    line(25,25,825,825);
+
+  a1 += 100;
+  stroke(c);
+  strokeWeight(4);
+  ellipse(a1, a2, a1, a2);
+
 }
