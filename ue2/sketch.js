fb4bd2d2409072d357512f63118bdf5f421ad903
Cheyenne Wäny <ch.waeny@gmail.com>
2020-09-19 13:28:56 +0200

    fix

--- ue2/sketch.js
+++ ue2/sketch.js
@@ -19,144 +19,80 @@ function draw() {
   strokeWeight(1);
   ellipse(a1, a2, a1, a2);
 
-  stroke('#654d7b');
-  strokeWeight(1);
-  line(25,25,825,825);
-
   a2 += 25;
   stroke(c);
   strokeWeight(2);
   ellipse(a1, a2, a1, a2);
-
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
 
   a1 += 30;
   stroke(d);
   strokeWeight(3);
   ellipse(a1, a2, a1, a2);
 
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
-
   a2 += 35;
   stroke(b);
   strokeWeight(4);
   ellipse(a1, a2, a1, a2);
-
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
 
   a1 += 40;
   stroke(c);
   strokeWeight(3);
   ellipse(a1, a2, a1, a2);
 
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
-
   a2 += 45;
   stroke(d);
   strokeWeight(2);
   ellipse(a1, a2, a1, a2);
-
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
 
   a1 += 50;
   stroke(b);
   strokeWeight(1);
   ellipse(a1, a2, a1, a2);
 
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
-
   a2 += 55;
   stroke(c);
   strokeWeight(2);
   ellipse(a1, a2, a1, a2);
-
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
 
   a1 += 60;
   stroke(d);
   strokeWeight(3);
   ellipse(a1, a2, a1, a2);
 
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
-
   a2 += 65;
   stroke(b);
   strokeWeight(1);
   ellipse(a1, a2, a1, a2);
-
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
 
   a1 += 70;
   stroke(c);
   strokeWeight(4);
   ellipse(a1, a2, a1, a2);
 
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
-
   a2 += 75;
   stroke(d);
   strokeWeight(3);
   ellipse(a1, a2, a1, a2);
-
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
 
   a1 += 80;
   stroke(b);
   strokeWeight(2);
   ellipse(a1, a2, a1, a2);
 
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
-
   a2 += 85;
   stroke(c);
   strokeWeight(1);
   ellipse(a1, a2, a1, a2);
-
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
 
   a1 += 90;
   stroke(d);
   strokeWeight(2);
   ellipse(a1, a2, a1, a2);
 
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
-
   a2 += 95;
   stroke(b);
   strokeWeight(3);
   ellipse(a1, a2, a1, a2);
-
-    stroke('#654d7b');
-    strokeWeight(1);
-    line(25,25,825,825);
 
   a1 += 100;
   stroke(c);
