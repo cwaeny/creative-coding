4cd2268f5fe8068b049940db76823ccdf6459953
Cheyenne Wäny <ch.waeny@gmail.com>
2020-10-16 18:39:33 +0200

    color changer

--- projekt_gui/sketch.js
+++ projekt_gui/sketch.js
@@ -18,6 +18,8 @@ var wahrscheinlichkeitMin = 1;
 var wahrscheinlichkeitMax = 100;
 var wahrscheinlichkeitSteps = 1;
 
+var jahreszeit = ['Herbst', 'Winter', 'Sommer'];
+
 /*  –––– –––– –––– –––– GUI Setup –––– –––– –––– ––––  */
 
 var gui;
@@ -27,7 +29,7 @@ function setup() {
   background('#F4F1F0');
 
   gui = createGui('Rainy days ☔');
-  gui.addGlobals('menge', 'windrichtung', 'wahrscheinlichkeit');
+  gui.addGlobals('menge', 'windrichtung', 'wahrscheinlichkeit', 'jahreszeit');
 
   //only call draw when then gui is changed
   noLoop();
@@ -38,9 +40,28 @@ function setup() {
 function draw() {
   colorMode(HSL);
   angleMode(DEGREES);
-  let mint = color(100, 24, 81);
-  let green = color(182, 50, 20);
-  let beige = color(25, 64, 68);
+
+  switch (jahreszeit) {
+    case 'Herbst' :
+      var first = color(100, 24, 81);
+      var second = color(182, 50, 20);
+      var third = color(25, 64, 68);
+      break;
+    case 'Winter' :
+      var first = color(13, 24, 81);
+      var second = color(102, 50, 20);
+      var third = color(125, 64, 68);
+      break;
+    case 'Sommer' :
+      var first = color(47, 24, 81);
+      var second = color(12, 50, 20);
+      var third = color(190, 64, 68);
+      break;
+  }
+
+  // let first = color(100, 24, 81);
+  // let second = color(182, 50, 20);
+  // let third = color(25, 64, 68);
 
   let size = wahrscheinlichkeit + 50;
   randomSeed(0); //let drops keep its color even when slider changes
@@ -52,19 +73,19 @@ function draw() {
       let randCol = int(random(0, 3));
       if (randCol == 0) {
         noFill();
-        stroke(green);
+        stroke(first);
         strokeWeight(2);
         square(0 + i, 0 + q, size, 0, size, size, size);
       }
       if (randCol == 1) {
         noFill();
-        stroke(mint);
+        stroke(second);
         strokeWeight(2);
         square(0 + i, 0 + q, size, 0, size, size, size);
       }
       if (randCol == 2) {
         noFill();
-        stroke(beige);
+        stroke(third);
         strokeWeight(2);
         square(0 + i, 0 + q, size, 0, size, size, size);
       }
